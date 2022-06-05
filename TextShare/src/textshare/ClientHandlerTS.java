30acd82b587a1013e23bb819395c2b7f02c25d7c
gabritech01 <centonze.gabri@gmail.com>
2022-06-05 17:09:12 +0200

    struttura concorrenza del textmanager implementata e comandi server edit create e read

--- TextShare/src/textshare/ClientHandlerTS.java
+++ TextShare/src/textshare/ClientHandlerTS.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-/** ClientHandler implementa la gestione della connessione con il suo client, è il "server vero e proprio".
+/** ClientHandler implementa la gestione della connessione con il suo client, ' il "server vero e proprio".
  * Prende il clientSocket per gestire la connessione con il client,
  * prende il TextManager che contiene i metodi per soddisfare le richieste del client,
  * prende la lista di Socket cosi' da rimuovere il socket che gestisce dalla socketList nel caso in cui il client richieda la disconnessione.
@@ -39,7 +39,24 @@ public class ClientHandlerTS implements Runnable {
                 	//Se il client chiude la connessione, fai lo stesso lato server
                 	System.out.println("Sto chiudendo il socket lato server");
                 	break;
-                } else {
+                } else if(command.equalsIgnoreCase("edit")){
+                    String nome =""; //nome del file che si vuole editare
+                    String testo=""; //testo del file editato
+                    while(!textManager.edit(nome, testo) && !textManager.owner()){ //controlla fino a quando non li dÃ  l'ok per editare
+                        textManager.edit(nome, testo);
+                    }  
+                }else if(command.equalsIgnoreCase("read")){
+                    String nome =""; //nome del file che si vuole editare
+                    while(textManager.read(nome) == "False" && !textManager.owner()){ //controlla fino a quando non li dÃ  l'ok per leggere
+                        System.out.println(textManager.read(nome));
+                    }
+                }else if(command.equalsIgnoreCase("create")){
+                    String nome =""; //nome del file che si vuole creare
+                    while(textManager.read(nome) == "False" && !textManager.owner()){
+                        System.out.println(textManager.read(nome));
+                    }
+                }
+                else {
                 	//In caso di comando sconosciuto, notifica il client
                 	toClient.println("Comando sconosciuto");
                 }
@@ -62,8 +79,8 @@ public class ClientHandlerTS implements Runnable {
             e.printStackTrace();
         } catch (NoSuchElementException e) {
         	/*
-        	 * Viene sollevata una NoSuchElementException quando il socket viene chiuso, perché fromClient.nextLine(), riga 28, non può più leggere
-        	 * Visto che il socket e' chiuso, il ClientHandler può terminare
+        	 * Viene sollevata una NoSuchElementException quando il socket viene chiuso, perch' fromClient.nextLine(), riga 28, non pu' pi' leggere
+        	 * Visto che il socket e' chiuso, il ClientHandler pu' terminare
         	 */
         	return;
         }
--- TextShare/src/textshare/TextManager.java
+++ TextShare/src/textshare/TextManager.java
@@ -1,9 +1,83 @@
 package textshare;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.concurrent.locks.ReentrantLock;
+//import java.util.Map;
+//import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;  
+import java.io.FileNotFoundException;
 /*
  * Contenitore dei metodi per gestire le operazioni sui file
  */
 public class TextManager {
+
+    /*questo serve per implementare un lock per ogni file e sarà sufficente generare 
+    un lock nel momento in cui viene richiesto l'editing di un file */
+    // Map<FileWriter, ReentrantLock> map = new HashMap<FileWriter, ReentrantLock>(); 
+    
+    ReentrantLock lockEdit = new ReentrantLock();
+    
+    public void create(String Filename){
+        try {
+            FileWriter file = new FileWriter(Filename + ".txt");
+            BufferedWriter output = new BufferedWriter(file);
+            output.close();
+            System.out.println("File creato con successo!");
+          }
+      
+          catch (Exception e) {
+            e.getStackTrace();
+          }
+    }
+
+      //questo metodo ha concorrenza, un solo utente per volta può editare.
+      public boolean edit(String name, String data){
+        if(! lockEdit.isLocked()){
+            lockEdit.lock(); //il thread acquisisce il lock
+            try {
+                FileWriter file = new FileWriter("output.txt");
+                BufferedWriter output = new BufferedWriter(file);
+                output.write(data);
+                // Closes the writer
+                output.close();
+            }catch (Exception e) {
+                e.getStackTrace();
+            }finally{
+                lockEdit.unlock(); //il thread rilascia il lock
+            }
+            return true;
+        }else{
+            System.out.print("Devi attendere che un lock si liberi");
+            return false;
+        }
+    }
+
+    public String read(String name){
+        if(lockEdit.isLocked()){
+            return "False";
+        }else if(! lockEdit.isLocked()) {
+            try {
+                File myObj = new File(name + ".txt");
+                Scanner myReader = new Scanner(myObj);
+                String data = "";
+                while (myReader.hasNextLine()) {
+                  data =  data + "\n" + myReader.nextLine();
+                }
+                myReader.close();
+                return data;
+              } catch (FileNotFoundException e) {
+                return "An error occurred.";
+              }
+            }
+        return "";
+    }
+
+    //ritorna un boleano a seconda se il thread che lo richiede è il possessore del lock o meno
+    public boolean owner(){
+        return lockEdit.isHeldByCurrentThread();
+    }
 
 
 }
